d9ae33d1db4749b8bd9e4cb1e191ada9307606a8
MwendeB <bettymwende201@gmail.com>
2023-08-13 13:42:43 -0700

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point for the program
+ * Description: A program that prints all possible
+ *              combinations of two two-digit numbers
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	return (0);
+}
