d2b5d916fe9bfc3a3da57003a42ae4329dc47d89
MwendeB <bettymwende201@gmail.com>
2023-08-13 12:48:41 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,35 +2,38 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point of the program
- * Description: A program that will assign a random number
- *              to the variable n each time it is executed.
- * The variable n will store a different value every
- * time you will run this program
- * if the number is greater than 0: is positive
- * if the number is 0: is zero
- * if the number is less than 0: is negative
+ * main - Entry point of this program
+ * Description: program will assign a random number to the
+ *              variable n each time it is executed.
+ * The variable n will store a different value every time
+ * you run this program.
+ * The string Last digit of, followed by
+ * n, followed by
+ * the string is, followed by
+ * if the last digit of n is greater than 5: the string
+ * and is greater than 5.
+ * if the last digit of n is 0: the string and is 0.
+ * if the last digit of n is less than 6 and not 0:
+ * the string and is less than 6 and not 0.
  * Return: 0 (Success)
  */
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
+	if (n % 10  > 5)
 	{
-		printf("%d is positive\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
-	else if (n == 0)
+	else if (n % 10 == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
-	else
+	else if (n % 10 < 6 && n % 10 != 0)
 	{
-		printf("%d is negative\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 
 	return (0);
